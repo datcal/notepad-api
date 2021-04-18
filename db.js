2832055feab080df1ef0327d50ccea69be17c2bd
Burak Hamza <thedatcal@gmail.com>
2021-04-18 19:48:00 +0300

    edit env file

--- db.js
+++ db.js
@@ -6,6 +6,20 @@ module.exports = () => {
 
    const HASAN = process.env.HASAN;
    console.log("HASAN : ",HASAN);
+   
+   const DB_USERNAME = process.env.DB_USERNAME;
+   console.log("DB_USERNAME : ",DB_USERNAME);
+
+   const DB_PASSWORD = process.env.DB_PASSWORD;
+   console.log("DB_PASSWORD : ",DB_PASSWORD);
+
+   const TEST1 = process.env.TEST1;
+   console.log("TEST1 : ",TEST1);
+
+   const TEST2 = process.env.TEST2;
+   console.log("TEST2 : ",TEST2);
+
+
     mongoose.connect(uri, {useFindAndModify: false,useCreateIndex : true,useNewUrlParser: true, useUnifiedTopology: true});
 
   mongoose.connection.on('open', () => {
