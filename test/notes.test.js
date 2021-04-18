4dec04f98af393a0b874933dfc6e75433bfe37e4
Burak Hamza <thedatcal@gmail.com>
2021-04-18 18:40:22 +0300

    chai enable timeouts set false

--- test/notes.test.js
+++ test/notes.test.js
@@ -8,7 +8,7 @@ let token, noteID;
 
 describe('notes test', () => {
 	before((done) => {
-        this.enableTimeouts(false)
+        chai.enableTimeouts(false)
 		chai.request(server)
 			.post('/users/login')
 			.send({mail: 'thedatcal@gmail.com', password: '123456'})
