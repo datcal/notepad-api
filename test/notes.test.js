0b9e9b6bb7ade0a6ee781888bf6c093b584f9643
Burak Hamza <thedatcal@gmail.com>
2021-04-18 17:39:57 +0300

    add tests

---
+++ test/notes.test.js
@@ -0,0 +1,100 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+const server = require('../app');
+chai.use(chaiHttp);
+
+let token, noteID;
+
+describe('notes test', () => {
+	before((done) => {
+		chai.request(server)
+			.post('/users/login')
+			.send({mail: 'thedatcal@gmail.com', password: '123456'})
+			.end((err, res) => {
+				token = res.body.token;
+				done();
+			});
+	});
+
+	describe('/notes/GET notes', () => {
+		it('it should GET all the notes', (done) => {
+			chai.request(server)
+				.get('/notes')
+				.set('x-access-token', token)
+				.end((err, res) => {
+					res.should.have.status(200);
+					res.body.should.be.a('array');
+					done();
+				});
+		})
+	});
+
+	describe('/notes/POST note', () => {
+		it('it should POST a note', (done) => {
+			const note = {
+				content: 'Lorem Ipsum',
+			};
+
+			chai.request(server)
+				.post('/notes')
+				.send(note)
+				.set('x-access-token', token)
+				.end((err, res) => {
+					res.should.have.status(200);
+					res.body.should.be.a('object');
+					res.body.should.have.property('content');
+					noteID = res.body._id;
+					done();
+				});
+		});
+	});
+
+	describe('/GET/notes/:noteID note', () => {
+		it('it should GET a note by the given id', (done) => {
+			chai.request(server)
+				.get('/notes/' + noteID)
+				.set('x-access-token', token)
+				.end((err, res) => {
+					res.should.have.status(200);
+					res.body.should.be.a('object');
+					res.body.should.have.property('content');
+					res.body.should.have.property('_id').eql(noteID);
+					done();
+				});
+		});
+	});
+
+	describe('/PUT/notes/:noteID movie', () => {
+		it('it should UPDATE a note given by id', (done) => {
+			const note = {
+				content: 'LOREM IPSUM',
+			};
+
+			chai.request(server)
+				.put('/notes/' + noteID)
+				.send(note)
+				.set('x-access-token', token)
+				.end((err, res) => {
+					res.should.have.status(200);
+					res.body.should.be.a('object');
+					res.body.should.have.property('content').eql(note.content);
+					done();
+				});
+		});
+	});
+
+	describe('/DELETE/notes/:noteID note', () => {
+		it('it should DELETE a note given by id', (done) => {
+			chai.request(server)
+				.delete('/notes/' + noteID)
+				.set('x-access-token', token)
+				.end((err, res) => {
+					res.should.have.status(200);
+					res.body.should.be.a('object');
+					res.body.should.have.property('status').eql(1);
+					done();
+				});
+		});
+	});
+});
