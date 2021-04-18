9e5c163410184c959f768154a6ef2803a735bd97
Burak Hamza <burakhamza@mail.ru>
2021-04-18 16:54:03 +0300

    add user route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,72 @@
 var express = require('express');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const User = require('../models/User');
+
+/* POST user register. */
+router.post('/register', function(req, res, next) {
+  const { mail, password, fullname } = req.body;
+
+  bcryptjs.hash(password, 10).then((hash) => {
+    const user = new User({
+      mail,
+      password:hash,
+      fullname
+    });
+  
+    const promise = user.save();
+    promise.then((data) =>{
+      res.status(200).json({'status' : 'succes', 'message' : 'Registration is successfully', 'code' : ''});
+    }).catch((err) => {
+      res.status(400).json({'status' : 'error', 'message' : 'Registration failed.', 'code' : '0990'});
+    });
+
+  });
+
+
+});
+
+/* POST user login. */
+router.post('/login', function(req, res, next) {
+  const { mail , password } = req.body;
+
+  User.findOne(
+    {
+      mail
+    },
+      (err,user) => {
+          if(err){
+            res.status(400).json({'status':'error','message' : 'Authentication failed, wrong password.','code':'09977'});
+          }
+
+          if(!user){
+            res.status(401).json({'status':'error','message' : 'Authentication failed, wrong password.','code':'09975'});
+          }  
+          
+          bcryptjs.compare(password, user.password).then((result) => {
+
+              if(!result){
+                res.status(400).json({'status':'error','message' : 'Authentication failed, wrong password.','code':'09976'});
+              }else{
+                
+                const payload = {
+                  mail
+                };
+                
+                const API_SECRET_KEY = process.env.API_SECRET_KEY;
+                const token = jwt.sign(payload, API_SECRET_KEY, {
+                  expiresIn: 720 // 12 saat
+                });
+
+                res.status(200).json({'status' :  'success' , token});
+
+              }
+          }); 
+      }
+    );
+
 });
 
 module.exports = router;
